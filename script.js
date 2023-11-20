bae77051b4e84296e0e8678fe63036916ae3a919
Lucas Biazon Palma <lucasbiazonpalma@gmail.com>
2023-11-20 19:24:30 -0300

    Creating buttons

--- script.js
+++ script.js
@@ -1,73 +1,42 @@
-
 const btClock = document.querySelector("#btClock")
-const btTemporizador = document.querySelector("#btTemporizador")
-const btCronometro = document.querySelector("#btCronometro")
+const btTimer = document.querySelector("#btTimer")
+const btStopwatch = document.querySelector("#btStopwatch")
 
 const clock = document.querySelector("#clock")
-const temporizador = document.querySelector("#temporizador")
-const cronometro = document.querySelector("#cronometro")
+const timer = document.querySelector("#timer")
+const stopwatch = document.querySelector("#stopwatch")
 
-const atual = clock
+let mainAtual = clock
+function ClickBtClock(){
+    btClock.classList.remove("desativado")
+    btClock.classList.add('ativado')
+    btStopwatch.classList.add("desativado")
+    btTimer.classList.add("desativado")
 
-btClock.addEventListener("click", () => {
-    btClock.classList.remove("desativo")
-    btClock.classList.add("ativo")
-    btCronometro.classList.add("desativo")
-    btTemporizador.classList.add("desativo")
-
-    clock.classList.remove("disabled")
-    temporizador.classList.add("disabled")
-    cronometro.classList.add("disabled")
-
-    oi = setInterval(Relogio, 1000)
-})
-
-btTemporizador.addEventListener("click", () => {
-    btTemporizador.classList.remove("desativo")
-    btTemporizador.classList.add("ativo")
-    btClock.classList.add("desativo")
-    btCronometro.classList.add("desativo")
-    
-    temporizador.classList.remove("disabled")
-    clock.classList.add("disabled")
-    cronometro.classList.add("disabled")
-    
-    clearInterval(oi)
-  
-})
-
-btCronometro.addEventListener("click", () => {
-    btCronometro.classList.remove("desativo")
-    btCronometro.classList.add("ativo")
-    btClock.classList.add("desativo")
-    btTemporizador.classList.add("desativo")
-    
-    cronometro.classList.remove("disabled")
-    temporizador.classList.add("disabled")
-    clock.classList.add("disabled")
-    clearInterval(oi)
-})
-
-const hora = document.querySelector("#ClockHora")
-    const min = document.querySelector("#ClockMin")
-    const seg = document.querySelector("#ClockSeg")
-    const date = document.querySelector("#date")
-function Relogio(){
-    const horarioDate = new Date()
-    const horaDate = horarioDate.getHours().toString().padStart(2, '0')
-    const minDate = horarioDate.getMinutes().toString().padStart(2, '0')
-    const segDate = horarioDate.getSeconds().toString().padStart(2, '0')
-
-    
-    date.textContent = `${horarioDate.getDate().toString().padStart(2, '0')}/${horarioDate.getMonth().toString().padStart(2, '0')}/${horarioDate.getFullYear().toString().padStart(2, '0')} `
-    hora.textContent = horaDate
-    min.textContent = minDate
-    seg.textContent = segDate
-
-    
+    mainAtual.classList.add("hidden")
+    clock.classList.remove("hidden")
+    mainAtual = clock
 }
 
-oi  = setInterval(Relogio, 1000)
+function ClickBtTimer(){
+    btTimer.classList.remove("desativado")
+    btTimer.classList.add("ativado")
+    btClock.classList.add("desativado")
+    btStopwatch.classList.add("desativado")
 
+    mainAtual.classList.add("hidden")
+    timer.classList.remove("hidden")
+    mainAtual = timer
+}
 
+function ClickBtStopwatch(){
+    btStopwatch.classList.remove("desativado")
+    btStopwatch.classList.add("ativado")
+    btClock.classList.add("desativado")
+    btTimer.classList.add("desativado")
+
+    mainAtual.classList.add("hidden")
+    stopwatch.classList.remove("hidden")
+    mainAtual = stopwatch
+}
 
